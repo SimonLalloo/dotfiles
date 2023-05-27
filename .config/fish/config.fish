d52cc8531a695d95367b1b835338c4d7c054547c
SimonLalloo <simon@lalloo.net>
2023-05-27 15:16:05 +0200

    Add tmux

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,12 +6,15 @@ end
 alias :e='nvim'
 alias c='clear'
 alias eduroam='nmcli device wifi connect eduroam'
-alias ssh_school='ssh sila3085@arrhenius.it.uu.se'
 alias ll='ls -alF'
 alias updateall='sudo pacman -Syu && paru -Syu && flatpak update'
 
+# SSH 
+alias ssh_start='eval $(ssh-agent -c)'
+alias ssh_school='ssh sila3085@arrhenius.it.uu.se'
+
 # Git aliases
-alias gg='git status'
+alias gg='git status -sb'
 alias gd='git difftool'
 
 # === Welcome message ===
@@ -19,3 +22,12 @@ set fish_greeting
 
 # === Prompt ===
 starship init fish | source
+
+# === Plugins ===
+# Run these in fish
+# TODO: Make this part of the config
+# curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+
+# Auto-start tmux
+# fisher install budimanjojo/tmux.fish
+set -Ux fish_tmux_config $HOME/.config/tmux/tmux.conf
