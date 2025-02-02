b574e36fec11f2a60d8b85160f69a32132ca1305
Simon Lalloo <simon@lalloo.me>
2025-02-02 17:03:17 +0100

    Update blink.cmp

--- nvim/.config/nvim/lua/custom/plugins/blink.lua
+++ nvim/.config/nvim/lua/custom/plugins/blink.lua
@@ -1,44 +1,48 @@
 return {
-  'saghen/blink.cmp',
-  lazy = false, -- lazy loading handled internally
-  -- optional: provides snippets for the snippet source
-  dependencies = 'rafamadriz/friendly-snippets',
+  {
+    'saghen/blink.cmp',
+    dependencies = 'rafamadriz/friendly-snippets',
 
-  -- use a release tag to download pre-built binaries
-  version = 'v0.*',
+    version = '*',
 
-  ---@module 'blink.cmp'
-  ---@type blink.cmp.Config
-  opts = {
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      -- 'default' for mappings similar to built-in completion
+      -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+      -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+      -- See the full "keymap" documentation for information on defining your own keymap.
+      keymap = {
+        preset = 'default',
 
-    keymap = {
-      preset = 'default',
-      ['<C-n>'] = { 'select_next', 'fallback' },
-      ['<Tab>'] = { 'select_next', 'fallback' },
-      ['<C-p>'] = { 'select_prev', 'fallback' },
-      ['<S-Tab>'] = { 'select_prev', 'fallback' },
+        ['<C-n>'] = { 'select_next', 'fallback' },
+        ['<Tab>'] = { 'select_next', 'fallback' },
+        ['<C-p>'] = { 'select_prev', 'fallback' },
+        ['<S-Tab>'] = { 'select_prev', 'fallback' },
 
-      ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
-      ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+        ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+        ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
 
-      ['<C-y>'] = { 'select_and_accept' },
-      ['<CR>'] = { 'select_and_accept', 'fallback' },
+        ['<C-y>'] = { 'select_and_accept' },
+        -- ['<CR>'] = { 'select_and_accept', 'fallback' },
+      },
+
+      appearance = {
+        -- Sets the fallback highlight groups to nvim-cmp's highlight groups
+        -- Useful for when your theme doesn't support blink.cmp
+        -- Will be removed in a future release
+        use_nvim_cmp_as_default = true,
+        -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+        -- Adjusts spacing to ensure icons are aligned
+        nerd_font_variant = 'mono',
+      },
+
+      -- Default list of enabled providers defined so that you can extend it
+      -- elsewhere in your config, without redefining it, due to `opts_extend`
+      sources = {
+        default = { 'lsp', 'path', 'snippets', 'buffer' },
+      },
     },
-
-    highlight = {
-      -- sets the fallback highlight groups to nvim-cmp's highlight groups
-      -- useful for when your theme doesn't support blink.cmp
-      -- will be removed in a future release, assuming themes add support
-      use_nvim_cmp_as_default = true,
-    },
-    -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-    -- adjusts spacing to ensure icons are aligned
-    nerd_font_variant = 'mono',
-
-    -- experimental auto-brackets support
-    accept = { auto_brackets = { enabled = true } },
-
-    -- experimental signature help support
-    trigger = { signature_help = { enabled = true } },
+    opts_extend = { 'sources.default' },
   },
 }
